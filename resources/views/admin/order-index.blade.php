cf146bd55919c2befb52d1641ef44d81c91a8d6b
NazarH <soundcfavorite@gmail.com>
2023-09-18 18:14:05 +0300

    Commit_9

--- resources/views/admin/order-index.blade.php
+++ resources/views/admin/order-index.blade.php
@@ -1,6 +1,39 @@
 @extends('layouts.admin')
 @section('content')
 
-        <x-admin.admin-table-orders :orders="$orders"></x-admin.admin-table-orders>
+    <div class="container">
+        <table class="table">
+            <tr>
+                <th>Номер замовлення</th>
+                <th>Інформація</th>
+                <th></th>
+            </tr>
+            @unless (empty($orders))
+                @foreach ($orders as $order)
+                    <tr>
+                        <td class="item-center">{{ $order->id }}</td>
+                        <td class="item-center">
+                            <a href="{{ route('admin.order.info', $order->id) }}">Переглянути</a>
+                        </td>
+                        <td class="table-buttons">
+                            @if ($order->verify === '0')
+                                <a href="{{ route('admin.order.accept', $order->id) }}">Підтвердити</a>
+                                <form action="{{ route('admin.order.delete', $order->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <input type="submit" value="Відхилити">
+                                </form>
+                            @else
+                                <form action="" method="post">
+                                    <input type="button" value="" disabled>
+                                </form>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            @endunless
+        </table>
+    </div>
+
 
 @endsection
--- resources/views/admin/tv-index.blade.php
+++
@@ -1,6 +0,0 @@
-<x-admin.admin-nav>
-    <x-slot:title>
-        OnlineStore - notebooks list
-        </x-slot>
-        <x-admin.admin-table :products="$product"></x-admin.admin-table>
-</x-admin.admin-nav>
