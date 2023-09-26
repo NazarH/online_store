32a23f7f0bbbc75338114bb9e2c4b5069d12ace7
NazarH <soundcfavorite@gmail.com>
2023-09-26 09:37:14 +0300

    Commit_24

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-include(base_path('routes/admin/admin.php'));
-include(base_path('routes/basket/basket.php'));
-include(base_path('routes/main/main.php'));
-include(base_path('routes/order/order.php'));
-include(base_path('routes/orders/orders.php'));
-include(base_path('routes/products/products.php'));
-include(base_path('routes/profile/profile.php'));
+include(base_path('routes/admin/web.php'));
+include(base_path('routes/basket/web.php'));
+include(base_path('routes/main/web.php'));
+include(base_path('routes/order/web.php'));
+include(base_path('routes/orders/web.php'));
+include(base_path('routes/products/web.php'));
+include(base_path('routes/profile/web.php'));
 
 
